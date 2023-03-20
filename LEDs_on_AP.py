e94ba00ad9e9482ef475937671c39ddc7d194ee3
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-03-19 21:36:29 -0500

    Update LEDs_on_AP.py

--- LEDs_on_AP.py
+++ LEDs_on_AP.py
@@ -144,4 +144,20 @@ while True:
             np[co2_skaleret+i] = (0, brightness, 0)
             np.write()
         co2_skaleret_old = co2_skaleret
+# use old measurement:
+    else: 
+        for i in range(co2_skaleret_old):
+            np[i] = (brightness, 0, 0)
+            np.write()
+        for i in range(30-co2_skaleret_old):
+            np[int(co2_skaleret_old)+i] = (0, brightness, 0)
+            np.write()
+        # blink LED no 1:
+        for i in range(showBlink):
+            np[1] = (0, 0, brightness)
+            np.write()
+            utime.sleep_ms(10)
+            np[1] = (brightness, 0, 0)
+            np.write()
+            utime.sleep_ms(590)
     
