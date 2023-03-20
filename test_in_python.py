ccb2a5cffb36685810bd64a9c2da0355a471a630
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-03-19 21:39:09 -0500

    Create test_in_python.py

---
+++ test_in_python.py
@@ -0,0 +1,14 @@
+#run: ctrl + shift + B in Atom-IDE
+
+
+import json
+import requests
+
+def get_carbon_intensity():
+    print ("loop")
+    requestUrl = 'https://api.co2signal.com/v1/latest?countryCode=DK-DK1'
+    headers = {'auth-token': 'TOKEN'}
+    response = requests.get(requestUrl, headers=headers)
+    dataObject = response.json()
+
+
