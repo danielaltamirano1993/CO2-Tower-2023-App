83ebaf25af48b96274f2d65717d65b22e82f39e7
danielaltamiranoborja <64813513+danielaltamirano1993@users.noreply.github.com>
2023-03-19 21:39:25 -0500

    Update test_in_python.py

--- test_in_python.py
+++ test_in_python.py
@@ -10,6 +10,15 @@ def get_carbon_intensity():
     headers = {'auth-token': 'TOKEN'}
     response = requests.get(requestUrl, headers=headers)
     dataObject = response.json()
+    if not isinstance(dataObject, dict) and "data" not in dataObject:
+        return 0
+    if "carbonIntensity" not in dataObject['data']:
+        return 0
+    try:
+        float(dataObject['data']['carbonIntensity'])
+    except ValueError:
+        return 0
+    return dataObject['data']['carbonIntensity']
 
 
 co2_level = get_carbon_intensity()
